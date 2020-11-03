c9dd0a85d5c48e3ef9aba6f38f0cbdd056f70f2d
Lipsky <liylipsky@gmail.com>
2020-11-03 15:31:31 +0800

    Triangle Done

--- OpenGLExcercise02_Triangle/OpenGLExcercise01/main.cpp
+++ OpenGLExcercise02_Triangle/OpenGLExcercise01/main.cpp
@@ -53,14 +53,24 @@
 
 #include <iostream>
 #define GLEW_STATIC
-#include <GL/glew.h>		
-#include <GLFW/glfw3.h>
-using namespace std;
-
+#include <GL/glew.h>			 
+#include <GLFW/glfw3.h>			 
+using namespace std;			 
+								 
 float vertices[] = {
-	-0.5f, -0.5f, 0.0f,
-	 0.5f, -0.5f, 0.0f,
-	 0.0f,  0.5f, 0.0f
+	-0.5f, -0.5f, 0.0f,				// 0 
+	 0.5f, -0.5f, 0.0f,				// 1
+	-0.5f,  0.5f, 0.0f,				// 2
+/*	 0.5f, -0.5f, 0.0f,
+	 0.0f,  0.5f, 0.0f,		*/
+	 0.5f, 0.5f, 0.0f			// 3
+};
+
+//0, 1, 2		0, 2, 3
+unsigned int indices[] = { // 注意索引从0开始! 
+	0, 1, 2, // 第一个三角形
+	//1, 2, 3  // 第二个三角形
+	2, 1, 3  // 第二个三角形
 };
 
 const char* vertexShaderSource =
@@ -86,7 +96,7 @@ int main() {
 
 	//open GLFW Window
 	//create a window object, which will required by most of other functions of GLFW.
-	GLFWwindow* window = glfwCreateWindow(800, 600, "Mywindow", NULL, NULL);
+	GLFWwindow* window = glfwCreateWindow(800, 600, "Triangle", NULL, NULL);
 	if (window == NULL)
 	{
 		cout << "Open window failed" << endl;
@@ -111,11 +121,16 @@ int main() {
 	{
 		cout << "Init glew failed";
 		glfwTerminate();
-		return -1;
+		return -1; 
 	}
 	// the first two parameters set location of the lower left corner of the window 
 	//while the third and fourth set the width and height of the rendering window in pixels. 	
 	glViewport(0, 0, 800, 600);
+	glEnable(GL_CULL_FACE);
+	glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);			// draw line of triangle
+	//glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);		// draw fill
+	//glCullFace(GL_BACK);
+	//glCullFace(GL_FRONT);
 
 	// vertex array object
 	unsigned int VAO;
@@ -126,10 +141,16 @@ int main() {
 	unsigned int VBO;
 	glGenBuffers(1, &VBO);
 	glBindBuffer(GL_ARRAY_BUFFER, VBO);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+	unsigned int EBO;
+	glGenBuffers(1, &EBO);
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
 
 	// targeted to copy user-defined data into the currently bound buffer			
 	// copy vertices data into GPU bound buffer
-	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+	//glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 
 	unsigned int vertexShader;
 	vertexShader = glCreateShader(GL_VERTEX_SHADER);
@@ -148,7 +169,14 @@ int main() {
 	glAttachShader(shaderProgram, fragmentShader);
 	glLinkProgram(shaderProgram);
 
+	// tell OpenGL how it should interpret the vertex data
+	// 0: start location, which we specified the location of the position vertex attribute in vertex shader
+	// 3: every vertex attribute is a vec3 
+	// GL_FALSE: normalize the data? no
+	// fifth: stride, space between consecutive vertex attribute
+	// offset of where the position data begins in the buffer
 	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+	//  enable the vertex attribute
 	glEnableVertexAttribArray(0);
 
 
@@ -160,8 +188,10 @@ int main() {
 		glClear(GL_COLOR_BUFFER_BIT);
 
 		glBindVertexArray(VAO);
+		glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
 		glUseProgram(shaderProgram);
-		glDrawArrays(GL_TRIANGLES, 0, 3);
+		//glDrawArrays(GL_TRIANGLES, 0, 6);
+		glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
 
 		//swap the buffer with screen
 		glfwSwapBuffers(window);
@@ -173,16 +203,6 @@ int main() {
 
 	return 0;
 }
-
-
-
-
-
-
-
-
-
-
 
 
 
